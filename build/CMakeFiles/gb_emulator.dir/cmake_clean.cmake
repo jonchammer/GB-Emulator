02e65e37b8a7764d95083f8b5d1e739e71377225
Jon C. Hammer <jonhammer_2008@yahoo.com>
2016-05-15 11:30:21 -0500

    Test commit.

---
+++ build/CMakeFiles/gb_emulator.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gb_emulator.dir/src/CPU.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Emulator.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Graphics.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Input.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/main.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/MBC.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Memory.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/SDL_Environment.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Sound/SoundUnit1.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Sound/SoundUnit2.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Sound/SoundUnit3.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Sound/SoundUnit4.cpp.obj"
+  "CMakeFiles/gb_emulator.dir/src/Sound.cpp.obj"
+  "gb_emulator.pdb"
+  "gb_emulator.exe"
+  "gb_emulator.exe.manifest"
+  "libgb_emulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gb_emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
